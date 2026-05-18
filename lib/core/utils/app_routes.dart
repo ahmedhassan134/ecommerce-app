3dd65fe7147009d422cbb228526c1aea893380b4
Ahmed Hasan <aahmed.hhasn@gmail.com>
2026-05-18 22:28:39 +0300

    initilize app

---
+++ lib/core/utils/app_routes.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const String welcomeScreen="welcomeScreen";
+  static const String loginScreen="loginScreen";
+  static const String signupScreen="signupScreen";
+  static const String homeScreen="homeScreen";
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:ecommerce/core/utils/app_routes.dart';
+import 'package:ecommerce/ui/screens/auth/login_screen/screens/login_screen.dart';
+import 'package:ecommerce/ui/screens/auth/signup_screen/screens/signup_screen.dart';
+import 'package:ecommerce/ui/screens/home_screen/screens/home_screen.dart';
+import 'package:ecommerce/ui/screens/welcom_screen/screens/welcom_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EcommerceApp());
+}
+
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.welcomeScreen,
+      routes: {
+        AppRoutes.welcomeScreen:(context)=>WelcomeScreen(),
+        AppRoutes.loginScreen:(context)=>LoginScreen(),
+        AppRoutes.signupScreen:(context)=>SignupScreen(),
+        AppRoutes.homeScreen:(context)=>HomeScreen(),
+      },
+
+    );
+  }
+}
+
+
